83ae7100b4a8ac8d3354cb5a899f229f8ddb8512
HaGaDDD <bozinandrey1@yandex.ru>
2021-06-08 14:30:03 +0300

    Add project files.

---
+++ Waifu_API/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Waifu_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = @"https://api.waifu.pics/";
+            
+
+            Console.WriteLine("sfw or nsfw?");
+
+            string type = Console.ReadLine();
+
+            List<string> catSFW = new List<string> { "waifu", "neko", "awoo", "bite", "dance"};
+            List<string> catNSFW = new List<string> { "waifu", "neko", "trap", "blowjob"  };
+
+            if (type == "sfw")
+            {
+                foreach (var c in catSFW)
+                    Console.Write(c + ", ");
+            }
+
+            else
+            {
+                foreach (var c in catNSFW)
+                    Console.Write(c + ", ");
+            }
+
+            string cat = Console.ReadLine();
+
+            var request = WebRequest.Create($"{url}{type}/{cat}");
+
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                var weatherForecast = JsonConvert.DeserializeObject<Root>(result);
+                Console.WriteLine(weatherForecast.url);
+            }
+        }
+    }
+}
